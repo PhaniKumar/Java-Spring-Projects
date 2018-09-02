33bee2c21c82cafd641d98d174763c77c44986e2
Phani Kumar <phanikumar>
2018-09-02 16:58:07 +0530

    Initial commit with project setup

---
+++ src/main/java/com/github/phanikumar/javaspring/javaspring/JavaspringApplication.java
@@ -0,0 +1,12 @@
+package com.github.phanikumar.javaspring.javaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringApplication.class, args);
+	}
+}
